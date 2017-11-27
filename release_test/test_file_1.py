98f2653243e16eb3e90df3bcc38db81b90291e04
Jeffery Smith <jeff@pazien.com>
2017-11-27 16:27:28 -0500

    Adding a new commit to develop branch

--- release_test/test_file_1.py
+++ release_test/test_file_1.py
@@ -1,15 +1,10 @@
 # 
 # test_file_1.py
 #
-# version: 1.1.3
-#
-#================================#
-# THIS IS A HOTFIX               #
-# Merging together fixes: 1.1.3  #
-#================================#
+# version: 1.2
 #
 #--------------------------------#
 # This is a new develop feature  #
-# Added to 1.1a1                 #
+# Added to 1.2a1 to become 1.2a2 #
 #--------------------------------#
 #
