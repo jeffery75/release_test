c3e47ecb7d55d92f27c69d3f7a4b27e09af87b8d
Jeffery Smith <jeff@pazien.com>
2017-12-14 13:27:05 -0500

    Setting up for another go

--- release_test/test_file_1.py
+++ release_test/test_file_1.py
@@ -1,6 +1,10 @@
 # 
 # test_file_1.py
 #
-# version: 1.2a3
-# New feature
+# This is a test file.  It has some wonderful code in it.
+# Below is a history of stuff done.
+#=========================================================================================
+#
+# For version: 1.2a3
+# New features have been added
 #
